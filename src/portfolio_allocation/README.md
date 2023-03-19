494dc90316948ef88a975916abc86f1677228b31
Manuel Canedo Tabares <mtabares@roku.com>
2023-03-19 12:57:50 +0000

    Added README.md, requirements, and formatted code.

---
+++ src/portfolio_allocation/README.md
@@ -0,0 +1,21 @@
+# Modern Portfolio Theory Visualization
+
+This project implements and visualizes the ideas of Modern Portfolio Theory (MPT) using historical stock data. MPT is an investment framework developed by Harry Markowitz in 1952, which aims to optimize the risk and return characteristics of a portfolio by analyzing the correlation among the assets. The project constructs an efficient frontier and identifies an optimal portfolio based on the Sharpe ratio.
+
+## Key Concepts
+
+1. Risk and Return: In the context of investments, risk is the potential variability of returns, and return is the expected gain or loss on an investment. Generally, higher-risk investments are associated with higher potential returns.
+2. Diversification: Diversification is an investment strategy aimed at reducing risk by allocating investments across various financial instruments, industries, or other categories. It reduces the impact of individual asset performance on the overall portfolio.
+3. Efficient Frontier: The efficient frontier is a curve that represents the set of portfolios with the highest expected return for a given level of risk. It illustrates the trade-off between risk and return and helps investors identify the optimal asset allocation.
+4. Sharpe Ratio: The Sharpe ratio is a measure of risk-adjusted return that compares an investment's excess return (return above the risk-free rate) to its volatility. A higher Sharpe ratio indicates better risk-adjusted performance.
+5. Optimal Portfolio: An optimal portfolio is a collection of investments that maximize the expected return for a given level of risk or minimize risk for a given level of expected return. In this project, the optimal portfolio is determined by maximizing the Sharpe ratio.
+
+## Methodology
+
+This project uses historical stock data to compute daily returns, mean returns, and the covariance matrix. These inputs are then used to construct the efficient frontier and find the optimal portfolio allocation through optimization techniques.
+
+The primary objective is to visualize the efficient frontier and identify the optimal portfolio allocation that maximizes the Sharpe ratio, demonstrating the application of Modern Portfolio Theory in real-world investment scenarios.
+
+## Disclaimer
+
+The project is for educational and illustrative purposes only. It should not be considered financial or investment advice. Historical performance is not indicative of future results, and any investment decision should be made after thorough research and consultation with a professional advisor.
--- src/portfolio_allocation/portfolio_allocation.py
+++ src/portfolio_allocation/portfolio_allocation.py
@@ -21,6 +21,15 @@ def calculate_covariance_matrix(daily_returns):
     return covariance_matrix
 
 
+def neg_sharpe_ratio(weights, mean_returns, cov_matrix, risk_free_rate):
+    portfolio_return = np.sum(mean_returns * weights) * 252
+    portfolio_std_dev = np.sqrt(
+        np.dot(weights.T, np.dot(cov_matrix, weights))
+    ) * np.sqrt(252)
+    sharpe_ratio = (portfolio_return - risk_free_rate) / portfolio_std_dev
+    return -sharpe_ratio
+
+
 def optimal_allocation(mean_returns, cov_matrix, risk_free_rate=0.02):
     num_assets = len(mean_returns)
     args = (mean_returns, cov_matrix, risk_free_rate)
@@ -37,17 +46,8 @@ def optimal_allocation(mean_returns, cov_matrix, risk_free_rate=0.02):
     return result.x
 
 
-def neg_sharpe_ratio(weights, mean_returns, cov_matrix, risk_free_rate):
-    portfolio_return = np.sum(mean_returns * weights) * 252
-    portfolio_std_dev = np.sqrt(
-        np.dot(weights.T, np.dot(cov_matrix, weights))
-    ) * np.sqrt(252)
-    sharpe_ratio = (portfolio_return - risk_free_rate) / portfolio_std_dev
-    return -sharpe_ratio
-
-
 def efficient_frontier(
-    mean_returns, cov_matrix, num_portfolios=10, risk_free_rate=0.02
+    mean_returns, cov_matrix, num_portfolios=10000, risk_free_rate=0.02
 ):
     np.random.seed(42)
     num_assets = len(mean_returns)
@@ -72,20 +72,19 @@ def main(stock_list, start_date, end_date):
     stock_data = fetch_stock_data(stock_list, start_date, end_date)
     daily_returns = calculate_daily_returns(stock_data)
 
-    # Reorder mean_returns and cov_matrix according to the stock_list
     mean_returns = daily_returns.mean()[stock_list]
-    cov_matrix = daily_returns.cov().reindex(stock_list, axis=0).reindex(stock_list, axis=1)
+    cov_matrix = (
+        daily_returns.cov().reindex(stock_list, axis=0).reindex(stock_list, axis=1)
+    )
 
     optimal_weights = optimal_allocation(mean_returns, cov_matrix)
 
-    # Calculate risk, return, and Sharpe Ratio for the optimal portfolio
     portfolio_return = np.sum(mean_returns * optimal_weights) * 252
     portfolio_std_dev = np.sqrt(
         np.dot(optimal_weights.T, np.dot(cov_matrix, optimal_weights))
     ) * np.sqrt(252)
     sharpe_ratio = (portfolio_return - 0.02) / portfolio_std_dev
 
-    # Get efficient frontier data
     frontier_data = efficient_frontier(mean_returns, cov_matrix)
 
     return (
@@ -95,30 +94,28 @@ def main(stock_list, start_date, end_date):
     )
 
 
-def display_frontier_table(stock_list, frontier_data, top_n=5):
-    # Sort frontier_data based on Sharpe Ratio (in descending order)
-    sorted_indices = np.argsort(frontier_data[2, :])[::-1]
-    sorted_frontier_data = frontier_data[:, sorted_indices]
-
-    # Select top_n portfolios
-    top_frontier_data = sorted_frontier_data[:, :top_n]
-
-    num_portfolios = top_frontier_data.shape[1]
-    data = {
-        "Risk": top_frontier_data[0, :],
-        "Return": top_frontier_data[1, :],
-        "Sharpe Ratio": top_frontier_data[2, :],
-    }
-
-    for i, stock in enumerate(stock_list):
-        data[stock] = top_frontier_data[3 + i, :]
-
-    df = pd.DataFrame(data).T
-    df.columns = [f"Portfolio {i + 1}" for i in range(num_portfolios)]
-    print(df)
+def plot_efficient_frontier(frontier_data, optimal_portfolio_metrics):
+    plt.figure(figsize=(10, 6))
+    plt.scatter(
+        frontier_data[0, :], frontier_data[1, :], c=frontier_data[2, :], cmap="viridis"
+    )
+    plt.colorbar(label="Sharpe Ratio")
+    plt.scatter(
+        optimal_portfolio_metrics[0],
+        optimal_portfolio_metrics[1],
+        c="red",
+        marker="*",
+        s=200,
+        label="Optimal Portfolio",
+    )
+    plt.xlabel("Volatility")
+    plt.ylabel("Return")
+    plt.title("Efficient Frontier")
+    plt.legend()
+    plt.show()
 
 
-stock_list = ["TSLA", "VEVE.L", "BRK-B", "MSFT"]
+stock_list = ["TSLA", "VEVE.L", "SPY", "BRK-B", "MSFT"]
 start_date = "2013-01-01"
 end_date = "2022-12-31"
 
@@ -129,23 +126,9 @@ print("Optimal weights:")
 for stock, weight in optimal_weights.items():
     print(f"{stock}: {weight:.6f}")
 
-# Print optimal portfolio metrics
 print("\nOptimal Portfolio Metrics:")
 print(f"Risk: {optimal_portfolio_metrics[0]:.6f}")
 print(f"Return: {optimal_portfolio_metrics[1]:.6f}")
 print(f"Sharpe Ratio: {optimal_portfolio_metrics[2]:.6f}")
 
-# Display the frontier table
-print("\nFrontier Portfolios:")
-display_frontier_table(stock_list, frontier_data)
-
-# Plot the efficient frontier
-plt.figure(figsize=(10, 6))
-plt.scatter(
-    frontier_data[0, :], frontier_data[1, :], c=frontier_data[2, :], cmap="viridis"
-)
-plt.colorbar(label="Sharpe Ratio")
-plt.xlabel("Volatility")
-plt.ylabel("Return")
-plt.title("Efficient Frontier")
-plt.show()
+plot_efficient_frontier(frontier_data, optimal_portfolio_metrics)
